1e5e5714d74fd2690dc2b91f3b7226bc965b383b
bilalmk <bilalmk@gmail.com>
2024-07-01 17:02:47 +0500

    working on user authentication

---
+++ authentication/app/main.py
@@ -0,0 +1,39 @@
+from typing import Annotated
+from shared.models.token import Token
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone
+from fastapi import FastAPI, Depends, status, HTTPException
+from app import config
+from jose import JWTError, jwt
+
+oauth2_authentication = OAuth2PasswordBearer(tokenUrl="token")
+app = FastAPI()
+@app.post("/generate_token", response_model=Token)
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    try:
+        access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": form_data.username}, expires_delta=access_token_expires
+        )
+        
+        return Token(
+            access_token=access_token,
+            token_type="bearer"
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_200_OK,
+            detail=str(e),
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+        
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
+    return encoded_jwt
