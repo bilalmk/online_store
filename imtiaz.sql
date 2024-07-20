9973fb18012c33ec7b57a98db5bc78cd08ddbd01
bilalmk <bilalmk@gmail.com>
2024-07-20 12:09:01 +0500

    payment record in db using kafka, after checkout and update the order status in order table

--- imtiaz.sql
+++ imtiaz.sql
@@ -11,157 +11,41 @@ MySQL - 8.0.38 : Database - imtiaz
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `addresses` */
+/*Data for the table `addresses` */
 
-CREATE TABLE `addresses` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `address` varchar(255) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `state` varchar(100) NOT NULL,
-  `postal_code` varchar(20) NOT NULL,
-  `country` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*Data for the table `alembic_version` */
 
-/*Table structure for table `alembic_version` */
+insert  into `alembic_version`(`version_num`) values ('165b091b7ccd');
 
-CREATE TABLE `alembic_version` (
-  `version_num` varchar(32) NOT NULL,
-  PRIMARY KEY (`version_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*Data for the table `brands` */
 
-/*Table structure for table `brands` */
+insert  into `brands`(`id`,`brand_name`,`brand_slug`,`guid`,`created_by`,`created_at`,`status`) values (1,'J.J','jj','893b5b31-f6e8-45ed-9e90-2d12b9703eda',1,'2024-07-18 04:34:47',1),(2,'Nike','nike','83e331f1-e130-4ed6-aa7f-cb35b50e24be',1,'2024-07-18 04:34:47',1),(3,'Dinners','dinners','b3da3cf4-7727-4486-ad31-f89bb28e6de8',1,'2024-07-18 04:34:47',1),(4,'Bata','bata','d0accc23-d359-4412-8811-c23198a36c58',1,'2024-07-18 04:34:47',1),(5,'Gul Ahmed','gul-ahmed','d451122f-0830-4585-a173-d0e1dc565e7f',1,'2024-07-18 04:34:47',1),(6,'Oxfort','oxfort','a3407e02-9cab-41ad-9196-d322910521b6',1,'2024-07-18 04:34:47',1);
 
-CREATE TABLE `brands` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `brand_name` varchar(255) NOT NULL,
-  `brand_slug` varchar(255) NOT NULL,
-  `guid` char(36) NOT NULL,
-  `created_by` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` tinyint NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `brand_name` (`brand_name`),
-  UNIQUE KEY `brand_slug` (`brand_slug`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*Data for the table `categories` */
 
-/*Table structure for table `categories` */
+insert  into `categories`(`id`,`parent_id`,`category_name`,`category_slug`,`guid`,`created_by`,`created_at`,`status`) values (1,0,'Man','man','08e13060-635b-4bf3-ac0e-2fee6d4de4fa',1,'2024-07-18 04:34:49',1),(2,0,'Women','women','2f1a05ea-92f6-4d75-a64e-424e2a99d424',1,'2024-07-18 04:34:49',1),(3,0,'Kids','kids','29667dd4-bb09-43e0-be03-10a19834a722',1,'2024-07-18 04:34:49',1),(4,1,'Cloths','cloths','38782729-3cce-4c5d-9e80-5aed4fadec7c',1,'2024-07-18 04:34:49',1),(5,2,'Cloths','cloths','5a99e7df-53a9-4971-bca1-10aa9b390118',1,'2024-07-18 04:34:49',1),(6,3,'Cloths','cloths','bdbeb744-cf5a-48ce-8c31-9cad381dec0b',1,'2024-07-18 04:34:49',1),(7,1,'Shoes','shoes','71670012-4027-4159-a7cb-23c841c2ec5e',1,'2024-07-18 04:34:49',1),(8,1,'Jackets','jackets','917b8f92-1367-49eb-91de-3a8bf49a8030',1,'2024-07-18 04:34:49',1);
 
-CREATE TABLE `categories` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `parent_id` int DEFAULT NULL,
-  `category_name` varchar(255) NOT NULL,
-  `category_slug` varchar(255) NOT NULL,
-  `guid` char(36) NOT NULL,
-  `created_by` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` tinyint NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `parent_id` (`parent_id`,`category_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*Data for the table `order_detail` */
 
-/*Table structure for table `order_detail` */
+insert  into `order_detail`(`id`,`order_guid`,`order_id`,`product_id`,`quantity`,`unit_price`,`discount`,`total_price`,`create_date`,`update_date`,`status`) values (1,'10645f3b-c2b8-47e2-acf9-1db5415bea0e',1,1,1,'3500.00','500.00','3000.00','2024-07-18 04:34:48','2024-07-18 04:34:48',1),(2,'10645f3b-c2b8-47e2-acf9-1db5415bea0e',1,2,2,'4500.00','500.00','8000.00','2024-07-18 04:34:48','2024-07-18 04:34:48',1),(3,'9c0c5ea1-c426-424b-94c1-b45a762b9888',2,2,1,'3000.00','500.00','2500.00','2024-07-18 04:34:48','2024-07-18 04:34:48',1),(4,'9c0c5ea1-c426-424b-94c1-b45a762b9888',2,5,2,'3500.00','0.00','7000.00','2024-07-18 04:34:48','2024-07-18 04:34:48',1),(5,'77c1d11e-877f-48be-9fac-ced4e0369b2e',3,2,1,'3000.00','500.00','2500.00','2024-07-18 07:49:30','2024-07-18 07:49:30',1),(6,'77c1d11e-877f-48be-9fac-ced4e0369b2e',3,5,2,'3500.00','0.00','7000.00','2024-07-18 07:49:30','2024-07-18 07:49:30',1);
 
-CREATE TABLE `order_detail` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `order_guid` char(36) NOT NULL,
-  `order_id` int NOT NULL,
-  `product_id` int NOT NULL,
-  `quantity` int NOT NULL,
-  `unit_price` decimal(10,2) NOT NULL,
-  `discount` decimal(10,2) DEFAULT NULL,
-  `total_price` decimal(10,2) DEFAULT NULL,
-  `create_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` smallint DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*Data for the table `orders` */
 
-/*Table structure for table `orders` */
+insert  into `orders`(`order_id`,`customer_id`,`total_amount`,`status`,`created_at`,`order_date`,`shipping_address`,`billing_address`,`payment_method`,`guid`,`payment_status`,`delivery_date`,`delivery_status`,`order_status`) values (1,1,'11000.00',1,'2024-07-18 04:34:48','2024-07-18 04:34:48','FB AREA','FB Area','card','10645f3b-c2b8-47e2-acf9-1db5415bea0e','paid',NULL,'pending','created'),(2,1,'9500.00',1,'2024-07-18 04:34:48','2024-07-18 04:34:48','FB AREA, The Comforts','FB Area, The Comforts','card','9c0c5ea1-c426-424b-94c1-b45a762b9888','unpaid',NULL,'pending','created'),(3,1,'9500.00',1,'2024-07-18 07:49:30','2024-07-18 07:49:30','FB AREA, The Comforts','FB Area, The Comforts','card','77c1d11e-877f-48be-9fac-ced4e0369b2e','unpaid',NULL,'pending','created');
 
-CREATE TABLE `orders` (
-  `order_id` int NOT NULL AUTO_INCREMENT,
-  `customer_id` int DEFAULT NULL,
-  `total_amount` decimal(10,2) NOT NULL,
-  `status` tinyint NOT NULL DEFAULT '1',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `order_date` timestamp NOT NULL,
-  `shipping_address` varchar(255) NOT NULL,
-  `billing_address` varchar(255) NOT NULL,
-  `payment_method` varchar(50) NOT NULL,
-  `guid` char(36) NOT NULL,
-  `payment_status` varchar(50) NOT NULL DEFAULT 'unpaid',
-  `delivery_date` timestamp NULL DEFAULT NULL,
-  `delivery_status` varchar(50) NOT NULL DEFAULT 'pending',
-  `order_status` varchar(50) NOT NULL DEFAULT 'process',
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*Data for the table `payments` */
 
-/*Table structure for table `payments` */
+insert  into `payments`(`id`,`order_id`,`customer_id`,`transaction_id`,`payment_gateway`,`amount`,`payment_date`,`status`) values (1,1,1,'80022244231','authorize.net',17,'2024-07-19 14:31:07',1);
 
-CREATE TABLE `payments` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `order_id` char(15) NOT NULL,
-  `customer_id` int NOT NULL,
-  `transaction_id` int NOT NULL,
-  `payment_gateway` char(255) NOT NULL,
-  `amount` float NOT NULL,
-  `payment_date` datetime NOT NULL,
-  `status` tinyint NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*Data for the table `products` */
 
-/*Table structure for table `products` */
+insert  into `products`(`id`,`name`,`description`,`price`,`stock_quantity`,`category_id`,`brand_id`,`image_name`,`guid`,`status`,`created_at`,`created_by`) values (1,'Nike Sports Shoes',NULL,'10000.00',10,7,2,NULL,'8ac75044-0034-4730-b7a2-35aeed6485e1',1,'2024-07-18 04:34:45',1),(2,'Bata Formal Shoes',NULL,'3000.00',100,7,4,NULL,'3024858e-b29a-435c-b233-ed5d824738f4',1,'2024-07-18 04:34:45',1),(3,'Bata Casual Shoes',NULL,'4000.00',50,7,4,NULL,'11b230ad-32cb-4b30-a930-58eaa15c4884',1,'2024-07-18 04:34:45',1),(4,'Shalwa Qameez',NULL,'6500.00',50,4,1,NULL,'e9bc57a2-c2a4-4dec-b97d-efaa9a4bddae',1,'2024-07-18 04:34:45',1),(5,'T-Shirts',NULL,'3500.00',50,4,3,NULL,'8de2c2e0-50b6-42d7-8355-e9709f67daf8',1,'2024-07-18 04:34:45',1),(6,'Formall Shirts',NULL,'4500.00',50,4,3,NULL,'9cef080d-d9c8-4590-9a9e-7bf665f9a324',1,'2024-07-18 04:34:45',1),(7,'Pants',NULL,'6000.00',50,4,3,NULL,'c8e0ae8d-3f38-46f4-93da-698855cc8882',1,'2024-07-18 04:34:45',1);
 
-CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(2000) NOT NULL,
-  `description` text,
-  `price` decimal(10,2) NOT NULL,
-  `stock_quantity` int NOT NULL,
-  `category_id` int NOT NULL,
-  `brand_id` int NOT NULL,
-  `image_name` varchar(1000) DEFAULT NULL,
-  `guid` char(36) NOT NULL,
-  `status` tinyint NOT NULL DEFAULT '1',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*Data for the table `shoppingcart` */
 
-/*Table structure for table `shoppingcart` */
+/*Data for the table `users` */
 
-CREATE TABLE `shoppingcart` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `product_id` int DEFAULT NULL,
-  `quantity` int NOT NULL,
-  `added_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `product_id` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-/*Table structure for table `users` */
-
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `phone_number` varchar(20) DEFAULT NULL,
-  `guid` char(36) NOT NULL,
-  `status` tinyint NOT NULL DEFAULT '1',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+insert  into `users`(`id`,`username`,`email`,`password`,`first_name`,`last_name`,`address`,`phone_number`,`guid`,`status`,`created_at`) values (1,'bilalmk','bilalmk@gmail.com','$2b$12$9I1LZUhfXy2O3iCBn41niuSUS6X/q/VHcPV9vOujSlUqfesJi9wSe','Bilal','Khan','FB Area','0321-7001054','0830f793-6a60-49e2-809f-b0bccb5a503e',1,'2024-07-12 11:47:20');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
